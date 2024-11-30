18f40639e5a42d2462a36da5a5fb073366b16703
godwinsamu <164744237+godwinsamu@users.noreply.github.com>
2024-11-30 14:10:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const navlinks = document.getElementById("navlinks");
+const menu = document.getElementById("mennu");
+menu.addEventListener("click",()=>{
+    navlinks.classList.toggle("slide");
+});
